81043e9e673aa7296d74d23ee57e3c0eaeb55c7b
ikngtty <ikngtty@gmail.com>
2020-09-11 20:32:12 +0900

    Year and month selectable

--- src/month-selector.tsx
+++ src/month-selector.tsx
@@ -4,29 +4,45 @@ import { Month } from './month'
 
 type Props = {}
 type State = {
-    month: number,
+    month: Date,
 }
 
 export class MonthSelector
     extends React.Component<Props, State> {
 
     state: State = {
-        month: 9
+        month: (() => {
+            const now = new Date()
+            return new Date(now.getFullYear(), now.getMonth())
+        })(),
     }
 
     render() {
         return <div>
             <span onClick={this.goToPreviousMonth}>◀</span>
-            <Month year='2020' month={this.state.month.toString()} />
+            <Month
+                year={this.state.month.getFullYear().toString()}
+                month={(this.state.month.getMonth() + 1)
+                    .toString().padStart(2, '0')} />
             <span onClick={this.goToNextMonth}>▶</span>
         </div>
     }
 
     goToPreviousMonth = () => {
-        this.setState((state) => ({ month: state.month - 1 }))
+        this.setState((state) => ({
+            month: new Date(
+                state.month.getFullYear(),
+                state.month.getMonth() - 1,
+            ),
+        }))
     }
 
     goToNextMonth = () => {
-        this.setState((state) => ({ month: state.month + 1 }))
+        this.setState((state) => ({
+            month: new Date(
+                state.month.getFullYear(),
+                state.month.getMonth() + 1,
+            ),
+        }))
     }
 }
