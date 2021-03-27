b52f4509442e2a12c05b478a5e59fe14b9c36838
ikngtty <ikngtty@gmail.com>
2021-03-27 23:30:20 +0900

    Lift up month data from month-selector to calender

--- src/calender.tsx
+++ src/calender.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Date } from './date'
+import { Date as Date_ } from './date'
 import { Day } from './day'
 import { MonthSelector } from './month-selector'
 
@@ -7,30 +7,71 @@ function Week(props: {
     dates: number[],
 }) {
     return <div>
-        {props.dates.map(date => <Date date={date.toString()} />)}
+        {props.dates.map(date => <Date_ date={date.toString()} />)}
     </div>
 }
 
-export function Calender() {
-    const weeks = [
-        [1, 2, 3, 4, 5, 6, 7],
-        [8, 9, 10, 11, 12, 13, 14],
-        [15, 16, 17, 18, 19, 20, 21],
-        [22, 23, 24, 25, 26, 27, 28],
-        [29, 30, 31],
-    ]
-    return <div>
-        <MonthSelector />
+type Props = {}
+type State = {
+    baseDate: Date,
+}
 
-        <Day day="日" />
-        <Day day="月" />
-        <Day day="火" />
-        <Day day="水" />
-        <Day day="木" />
-        <Day day="金" />
-        <Day day="土" />
-        <br />
+export class Calender
+    extends React.Component<Props, State> {
 
-        {weeks.map(week => <Week dates={week} />)}
-    </div>
+    constructor(props: Props) {
+        super(props)
+
+        const now = new Date()
+        this.state = {
+            baseDate: new Date(now.getFullYear(), now.getMonth()),
+        }
+    }
+
+    render() {
+        const weeks = [
+            [1, 2, 3, 4, 5, 6, 7],
+            [8, 9, 10, 11, 12, 13, 14],
+            [15, 16, 17, 18, 19, 20, 21],
+            [22, 23, 24, 25, 26, 27, 28],
+            [29, 30, 31],
+        ]
+
+        return <div>
+            <MonthSelector
+                baseDate={this.state.baseDate}
+                onGoToNextMonth={() => this.goToNextMonth()}
+                onGoToPreviousMonth={() => this.goToPreviousMonth()}
+            />
+
+            <Day day="日" />
+            <Day day="月" />
+            <Day day="火" />
+            <Day day="水" />
+            <Day day="木" />
+            <Day day="金" />
+            <Day day="土" />
+            <br />
+
+            {weeks.map(week => <Week dates={week} />)}
+        </div>
+    }
+
+    goToPreviousMonth() {
+        this.setState((state) => ({
+            baseDate: new Date(
+                state.baseDate.getFullYear(),
+                state.baseDate.getMonth() - 1,
+            ),
+        }))
+    }
+
+    goToNextMonth() {
+        this.setState((state) => ({
+            baseDate: new Date(
+                state.baseDate.getFullYear(),
+                state.baseDate.getMonth() + 1,
+            ),
+        }))
+    }
 }
--- src/month-selector.tsx
+++ src/month-selector.tsx
@@ -2,47 +2,17 @@ import React from 'react';
 
 import { Month } from './month'
 
-type Props = {}
-type State = {
-    month: Date,
-}
-
-export class MonthSelector
-    extends React.Component<Props, State> {
-
-    state: State = {
-        month: (() => {
-            const now = new Date()
-            return new Date(now.getFullYear(), now.getMonth())
-        })(),
-    }
-
-    render() {
-        return <div>
-            <span onClick={this.goToPreviousMonth}>◀</span>
-            <Month
-                year={this.state.month.getFullYear().toString()}
-                month={(this.state.month.getMonth() + 1)
-                    .toString().padStart(2, '0')} />
-            <span onClick={this.goToNextMonth}>▶</span>
-        </div>
-    }
-
-    goToPreviousMonth = () => {
-        this.setState((state) => ({
-            month: new Date(
-                state.month.getFullYear(),
-                state.month.getMonth() - 1,
-            ),
-        }))
-    }
-
-    goToNextMonth = () => {
-        this.setState((state) => ({
-            month: new Date(
-                state.month.getFullYear(),
-                state.month.getMonth() + 1,
-            ),
-        }))
-    }
+export function MonthSelector(props: {
+    baseDate: Date,
+    onGoToPreviousMonth: () => void,
+    onGoToNextMonth: () => void,
+}) {
+    return <div>
+        <span onClick={props.onGoToPreviousMonth}>◀</span>
+        <Month
+            year={props.baseDate.getFullYear().toString()}
+            month={(props.baseDate.getMonth() + 1)
+                .toString().padStart(2, '0')} />
+        <span onClick={props.onGoToNextMonth}>▶</span>
+    </div>
 }
