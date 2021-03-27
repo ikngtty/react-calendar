cb59cef35db5ad367f93075f0a2d9a10aa993f76
ikngtty <ikngtty@gmail.com>
2021-03-27 22:21:34 +0900

    Variable dates

--- src/calender.tsx
+++ src/calender.tsx
@@ -3,7 +3,22 @@ import { Date } from './date'
 import { Day } from './day'
 import { MonthSelector } from './month-selector'
 
+function Week(props: {
+    dates: number[],
+}) {
+    return <div>
+        {props.dates.map(date => <Date date={date.toString()} />)}
+    </div>
+}
+
 export function Calender() {
+    const weeks = [
+        [1, 2, 3, 4, 5, 6, 7],
+        [8, 9, 10, 11, 12, 13, 14],
+        [15, 16, 17, 18, 19, 20, 21],
+        [22, 23, 24, 25, 26, 27, 28],
+        [29, 30, 31],
+    ]
     return <div>
         <MonthSelector />
 
@@ -16,21 +31,6 @@ export function Calender() {
         <Day day="土" />
         <br />
 
-        <Date date="1" />
-        <Date date="2" />
-        <Date date="3" />
-        <Date date="4" />
-        <Date date="5" />
-        <Date date="6" />
-        <Date date="7" />
-        <br />
-
-        <Date date="8" />
-        <Date date="9" />
-        <Date date="10" />
-        <Date date="11" />
-        <Date date="12" />
-        <Date date="13" />
-        <Date date="14" />
+        {weeks.map(week => <Week dates={week} />)}
     </div>
 }
