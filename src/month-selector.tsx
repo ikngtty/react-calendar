1a73e68839b63bd2ec8d76986fe3cb6fb66f26b5
ikngtty <ikngtty@gmail.com>
2021-03-28 00:01:46 +0900

    Integrate Month into MonthSelector

--- src/month-selector.tsx
+++ src/month-selector.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 
-import { Month } from './month'
-
 export function MonthSelector(props: {
     baseDate: Date,
     onGoToPreviousMonth: () => void,
     onGoToNextMonth: () => void,
 }) {
+    const yearString = props.baseDate.getFullYear().toString()
+    const monthString = (props.baseDate.getMonth() + 1).toString().padStart(2, '0')
     return <div>
         <span onClick={props.onGoToPreviousMonth}>◀</span>
-        <Month
-            year={props.baseDate.getFullYear().toString()}
-            month={(props.baseDate.getMonth() + 1)
-                .toString().padStart(2, '0')} />
+        {yearString}年 {monthString}月
         <span onClick={props.onGoToNextMonth}>▶</span>
     </div>
 }
